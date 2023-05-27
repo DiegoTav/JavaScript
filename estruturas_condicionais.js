b49802a94057949a0ddf00beabf11c40152f5931
Diego Tavares <sdiego14599@gmail.com>
2023-05-27 18:45:17 -0300

    condicionais

---
+++ estruturas_condicionais.js
@@ -0,0 +1,19 @@
+const i = 10;
+
+if (i >= 18){
+    console.log('Voce é maior de idade');
+}else{
+    console.log('Voce é menor de idade');
+}
+
+console.clear()
+
+let m = 4;
+
+if(m >= 7){
+    console.log('Aprovado');
+} else if (m >=5){
+    console.log('Recuperação');
+}else{
+    console.log('Reprovado');
+}
