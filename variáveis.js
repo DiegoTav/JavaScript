0461f17c99c630720445ee651dbaca452d41b0e9
Diego Tavares <sdiego14599@gmail.com>
2023-05-27 13:27:20 -0300

    Variáveis

---
+++ variáveis.js
@@ -0,0 +1,33 @@
+/*
+Olá Mundo
+*/
+console.log('Hello, World!')
+
+
+//Variáveis
+
+let idade = 25; //Criando a primerira variável
+console.log(idade);
+
+// Tipos de variáveis
+
+idade = 30;             //number
+let numero = 30;        //number
+let altura = 1.77;      //number
+let nome = 'Diego';     //String
+let estudando = true;   //Boolean
+
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+let outra;
+console.log(outra);
+
+let nula = null;
+console.log(nula);
+
+var variável = 30;   //Antigo, não usar
+
+const constante = 100;  //não é possivel mudar o valor
