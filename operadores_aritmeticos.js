e39e11e2fbf0e525627e18e935916704be927981
Diego Tavares <sdiego14599@gmail.com>
2023-05-27 14:07:20 -0300

    Operadores Aritmeticos

---
+++ operadores_aritmeticos.js
@@ -0,0 +1,20 @@
+let num1 = 20;
+let num2 = 2;
+
+const soma = num1 + num2;
+
+//operacoes basicas
+console.log('Soma =', soma);
+console.log('Subtração =', num1 - num2);
+console.log('Multiplicação =', num1 * num2);
+console.log('Divisão =', num1 / num2);
+console.log('Exponenciação =', num1 ** num2);
+console.log('Resto da Divisao =', num1 % 3);
+
+//Precedencia
+console.log(3 + 7 * 3);
+
+//Biblioteca Math
+console.log(Math.PI);
+
+
