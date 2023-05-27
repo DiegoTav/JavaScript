6b67a9f126d9352a893e41b3f13c4c28c86e07f5
Diego Tavares <sdiego14599@gmail.com>
2023-05-27 14:19:51 -0300

    Operadores Booleanos

---
+++ operadores_booleanos.js
@@ -0,0 +1,25 @@
+const n = 10;
+
+console.log(n == 10);
+console.log(n > 20);
+
+console.log(n == 10);
+console.log(10 == '10');
+console.log(10 === '10');
+
+console.clear();
+
+console.log(10 != 10);
+console.log(10 != '10');
+console.log(10 !== '10');
+
+let i = 26;
+let cnh = true;
+
+const possoDirigir = i >= 18 && cnh === true;
+console.log('Posso dirigir? ',possoDirigir);
+
+/*
+OR ||
+NOT !
+*/
